83b308a67551bc13325e05d90839916091d3df3d
DevOps2025p <chakri.palaparthi@gmail.com>
2025-06-26 16:54:16 +0530

    conversation

---
+++ 02-conversation.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+
+echo " Trump:: Hey kim how are you?"
+echo " Kim:: I'm fine waht about you Trump?
+echo " Trump: nothing much I'm going to war are you interested"
+echo " Kim: I'm very much interested Trump"
+echo " Trum: Kim Thank you for your interst"
