2c7fb3fc746056c3723b000bdc41285aea4bf861
DevOps2025p <chakri.palaparthi@gmail.com>
2025-06-26 18:25:36 +0530

    conversation

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,9 +1,9 @@
 #!bin/bash
 
-Person1=jim
-Person2=modi
+Person1=$1
+Person2=$2
 
-echo " $Person1:: Hey kim how are you?"
+echo " $Person1:: Hey $Person2 how are you?"
 echo " $Person2:: I'm fine waht about you $Person1?"
 echo " $Person1:: nothing much I'm going to war are you interested"
 echo " $Person2:: I'm very much interested $Person1"
