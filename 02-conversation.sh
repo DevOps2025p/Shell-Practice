a7732123a4ca2270c7e71013d7715788a971c265
DevOps2025p <chakri.palaparthi@gmail.com>
2025-06-26 17:06:11 +0530

    conversation

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,7 +1,10 @@
 #!bin/bash
 
-echo " Trump:: Hey kim how are you?"
-echo " Kim:: I'm fine waht about you Trump?"
-echo " Trump:: nothing much I'm going to war are you interested"
-echo " Kim:: I'm very much interested Trump"
-echo " Trum:: Kim Thank you for your interst"
+Person1=Trump
+Person2=Kim
+
+echo " $Person1:: Hey kim how are you?"
+echo " $Person2:: I'm fine waht about you $Person1?"
+echo " $Person1:: nothing much I'm going to war are you interested"
+echo " $Person2:: I'm very much interested $Person1"
+echo " $Person1:: $Person2 Thank you for your interst"
